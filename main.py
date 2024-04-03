5761a48e1d2020dfcb9476998144eeb7bf97577a
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-04-03 16:49:51 -0700

    Added times

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ shared_data = {
         "horses": [0] * NUM_HORSES,
         "graphics": [""] * NUM_HORSES,
         "bets": [0] * NUM_HORSES,
-        "horses_done": [False] * NUM_HORSES,
+        "horses_done": [None] * NUM_HORSES,
         "finished": False,
         "winner": None,
         }
 
-def update_horse(i, mutex):
+def update_horse(i, mutex, start_time):
     while shared_data["horses"][i] < 20:
         mutex.acquire()
         shared_data["horses"][i] += 2
@@ -22,7 +22,7 @@ def update_horse(i, mutex):
         time.sleep(random.uniform(0.25, 1.5))
        # print(f"Horse {i} is at {shared_data['horses'][i]}")
         shared_data["graphics"][i] += ("-> ")
-    shared_data["horses_done"][i] = True
+    shared_data["horses_done"][i] = time.time() - start_time
     if not shared_data["winner"]:
         shared_data["winner"] = i
 
@@ -37,8 +37,10 @@ def print_race(shared_data):
             print(f"{i} | {shared_data["graphics"][i]}", end="")
             if shared_data["horses_done"][i]:
                 print(f" ** ", end = "")
-            if shared_data["winner"] == i:
-                print(f"Horse {i + 1} wins! ", end = "")
+                if shared_data["winner"] == i:
+                    print(f"Horse {i + 1} wins in {shared_data["horses_done"][i]:.2f}s!", end = "")
+                else:
+                    print(f"Done in {shared_data["horses_done"][i]:.2f}s.", end = "")
             print()
         if all(shared_data["horses_done"]):
             local_done = True
@@ -49,8 +51,9 @@ def print_race(shared_data):
 def main():
     mutex = threading.Lock()
     threads = []
+    start_time = time.time()
     for i in range(NUM_HORSES):
-        t = threading.Thread(target=update_horse, args=(i, mutex))
+        t = threading.Thread(target=update_horse, args=(i, mutex, start_time))
         threads.append(t)
         t.start()
 
